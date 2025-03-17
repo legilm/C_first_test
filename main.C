5639da0372f2da86d2a521f1b506c84294ad8737
Gilmar <ogilmar@protonmail.com>
2025-03-17 14:27:24 +0100

    first C file

--- main.C
+++ main.C
@@ -1,3 +1,17 @@
 int main(){
+int a = 5;
+int b,c;
+
+printf("&a = %p, a = %d\n", &a, a);
+printf("&b = %p, b = %d\n", &b, b);
+printf("&c = %p, c = %d\n\n", &c, c);
+
+b = 20;
+c = a + b;
+
+printf("&a = %p, a = %d\n", &a, a);
+printf("&b = %p, b = %d\n", &b, b);
+printf("&c = %p, c = %d\n\n", &c, c);
+
 return 0;
 }
